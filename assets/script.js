bee22da1309f25bfb035029349eb80eb6f073f3d
ryantixier <123503138+ryantixier@users.noreply.github.com>
2023-05-22 16:36:18 -0600

    (HTML) laid out skeleton for quiz structure; (JS) created vars 'definitions, syntax, bonus', wrote q1 for definitions, wrote q1 for syntax; included alert to be issued upon page open.

---
+++ assets/script.js
@@ -0,0 +1,98 @@
+// CODE QUIZ //
+
+// // INDECES
+// // INDECES
+// // INDECES
+//
+var definitions = [
+  {
+    question: "DOM is an ackronym for...",
+    choices: [
+      "Do-Over Machine",
+      "Dubious Origami Men",
+      "Document Object Model",
+      "Taco",
+    ],
+    answer: choices[2],
+  },
+  //   {
+  //     question: ,
+  //     choices:,
+  //     answer:,
+  //   }
+  //   {
+  //     question: ,
+  //     choices:,
+  //     answer:,
+  //   }
+  //   {
+  //     question: ,
+  //     choices:,
+  //     answer:,
+  //   }
+  //   {
+  //     question: ,
+  //     choices:,
+  //     answer:,
+  //   }
+];
+
+var syntax = [
+  {
+    question: "What is the appropriate syntax to denote an array?",
+    choices: [
+      "Taco",
+      "[ARRAY] i.e. Brackets",
+      "{ARRAY} i.e. Curly Brackets",
+      "(ARRAY) i.e. Parentheses",
+    ],
+    answer: choices[1],
+  },
+  //   {
+  //     question: ,
+  //     choices:,
+  //     answer:,
+  //   },
+  //   {
+  //     question: ,
+  //     choices:,
+  //     answer:,
+  //   },
+  //   {
+  //     question: ,
+  //     choices:,
+  //     answer:,
+  //   },
+  //   {
+  //     question: ,
+  //     choices:,
+  //     answer:,
+  //   },
+];
+
+var bonus = [];
+
+// // QUERY SELECTORS
+// // QUERY SELECTORS
+// // QUERY SELECTORS
+//
+
+// // VARIABLES
+// // VARIABLES
+// // VARIABLES
+//
+
+// // FUNCTIONS
+// // FUNCTIONS
+// // FUNCTIONS
+//
+
+// // COMMANDS
+// // COMMANDS
+// // COMMANDS
+//
+
+// Upon visiting the page, introductory alert
+alert(
+  "¡Bienvenidos! Ready to test your JavaScript knowledge? Click 'Start Quiz' and see if you can get the high score!"
+);
