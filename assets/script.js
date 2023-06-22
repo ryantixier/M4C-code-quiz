3945b12ddfb477b9b407eabb9191ec4aa491cc8e
ryantixier <123503138+ryantixier@users.noreply.github.com>
2023-06-21 18:42:19 -0600

    (SCRIPT.JS) finalized script for quiz functionalityl; (HIGHSCORES.HTML) completed localStorage functions and page content; (INDEX.HTML) finalized content.

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,32 @@
 // CODE QUIZ //
 
-// // QUERY SELECTORS
-// // QUERY SELECTORS
-// // QUERY SELECTORS
 //
+//
+// // QUERY SELECTORS
+// // QUERY SELECTORS
+// // QUERY SELECTORS
 
-// NOTE: variable bound to the event listener used to begin running the quiz
+// variable bound to the event listener used to begin running the quiz
 var startButton = document.getElementById("start");
 
-// NOTE: variables for each of the 3 HTML sections
+// variables for each of the 3 HTML sections
 var introduction = document.getElementById("introduction");
 var quiz = document.getElementById("quiz");
 var scoreForm = document.getElementById("score-form");
+var scoreTimer = document.getElementById("score");
+var userInfo = document.getElementById("user-info");
+var userScore = document.getElementById("user-score");
+var submitButton = document.getElementById("submit-score");
 
-// // INDECES/VARIABLES
-// // INDECES/VARIABLES
-// // INDECES/VARIABLES
 //
+//
+// // INDECES/VARIABLES
+// // INDECES/VARIABLES
+// // INDECES/VARIABLES
 
-// NOTE: quiz questions comprise of "definitions" questions, "syntax" questions, and "bonus" questions
+var score = 100;
+
+// quiz questions comprise of "definitions" questions, "syntax" questions, and "bonus" questions
 var definitions = [
   {
     question: "DOM is an ackronym for...",
@@ -33,12 +41,12 @@ var definitions = [
   {
     question: "1",
     selections: ["1", "2", "3", "4"],
-    answer: "2",
+    answer: 2,
   },
   {
     question: "2",
     selections: ["1", "2", "3", "4"],
-    answer: "4",
+    answer: 3,
   },
   //   {
   //     question: ,
@@ -61,18 +69,18 @@ var syntax = [
       "{ARRAY} i.e. Curly Brackets",
       "(ARRAY) i.e. Parentheses",
     ],
-    answer: "[ARRAY] i.e. Brackets",
+    answer: 1,
     // answer: 1,
   },
   {
     question: "1",
     selections: ["1", "2", "3", "4"],
-    answer: "2",
+    answer: 2,
   },
   {
     question: "2",
     selections: ["1", "2", "3", "4"],
-    answer: "4",
+    answer: 3,
   },
   // {
   //   //     question: ,
@@ -90,17 +98,17 @@ var bonus = [
   {
     question: "0",
     selections: ["1", "2", "3", "4"],
-    answer: "3",
+    answer: 1,
   },
   {
     question: "1",
     selections: ["1", "2", "3", "4"],
-    answer: "2",
+    answer: 0,
   },
   {
     question: "2",
     selections: ["1", "2", "3", "4"],
-    answer: "4",
+    answer: 0,
   },
   // {
   //   //     question: ,
@@ -111,37 +119,51 @@ var bonus = [
   //   //     question: ,
   //   //     selections:,
   //   //     answer:,
-]; // same syntax as above
+];
 
-// NOTE: question categories merged into a single variable
+// question categories merged into a single variable
 var allQuestions = [...definitions, ...syntax, ...bonus];
 
-// NOTE: sets the starting point of the each question catergory to the 0 index
-// ... : to be used for iteration through var allQuestions during quiz
+// sets the starting point of the each question catergory to the 0 index
+// ... to be used for iteration through var allQuestions during quiz
 var questionIndex = 0;
 
-// // FUNCTIONS
-// // FUNCTIONS
-// // FUNCTIONS
-//
+var timerInterval;
 
-// NOTE: function will run upon click (event)
+//
+//
+// // FUNCTIONS
+// // FUNCTIONS
+// // FUNCTIONS
+
+function setTime() {
+  // Sets interval in variable
+  timerInterval = setInterval(function () {
+    score--;
+    scoreTimer.textContent = score;
+
+    if (score <= 0) {
+      gameOver();
+      // Stops execution of action at set interval
+    }
+  }, 1000); // specifies interval; 1000 ms / 1 sec
+}
+
+// function will run upon click (event)
 function startQuiz(event) {
   event.preventDefault();
   introduction.classList.add("hide");
   // ... : will hide HTML introductory section
   quiz.classList.remove("hide");
   // ... : will reveal HTML quiz section
+  setTime();
   selectQuestion();
   // ... : triggers the JS function to run the quiz
 }
 
-// ADD NOTES // ADD NOTES // ADD NOTES //
-// ADD NOTES // ADD NOTES // ADD NOTES //
-// ADD NOTES // ADD NOTES // ADD NOTES //
-
 // js function to begin quiz
 function selectQuestion() {
+  scoreTimer.textContent = score;
   var currentQuestion = allQuestions[questionIndex];
   // will display a given question on the page
   document.getElementById("question").textContent = currentQuestion.question;
@@ -154,56 +176,71 @@ function selectQuestion() {
     btn.textContent = choice;
     btn.addEventListener("click", choiceClick);
   }
-  // console.log(currentQuestion);
 }
 
-// ADD NOTES // ADD NOTES // ADD NOTES //`
-// ADD NOTES // ADD NOTES // ADD NOTES //
-// ADD NOTES // ADD NOTES // ADD NOTES //
+// function to compare answer to choice, to move to next question, and to subtract time from score in the event of incorrect answer
 function choiceClick(event) {
   event.preventDefault();
   var choice = event.target.id;
-  var answerKey = allQuestions[questionIndex];
-  // console.log(choice)
-  // console.log(currentQues)
-  // console.log(typeof choice); // var choice was a string
-  // console.log(typeof currentQuestion.answer); // var currentQuestion.answer was a number
-  if (parseInt(choice) === answerKey.answer) {
+  var answerKey = allQuestions[questionIndex].answer;
+  if (parseInt(choice) === answerKey) {
     // parseInt to convert string to number value; if strictly equal...
     console.log("yayyyyy"); // ~it worked...~
-    selectQuestion(questionIndex++);
     // recalls function selectQuestion to bring up the next question in the quiz...
     // ... i.e. presents the following item in allQuestions object array
     // > no time subtracted <
   } else {
-    console.log("booooo");
-    selectQuestion(questionIndex++);
+    console.log("booooo"); // ~it worked...~
     // next question
+    score -= 10;
     // TO-DO: subtract 10 seconds
   }
+  questionIndex++;
+  // will run gameOver() function if score <= 0 or if all questions have been answered
+  if (score <= 0 || questionIndex >= allQuestions.length) {
+    gameOver();
+  } else {
+    selectQuestion(questionIndex);
+  }
 }
+
+//
+function gameOver() {
+  clearInterval(timerInterval);
+  quiz.classList.add("hide");
+  scoreForm.classList.remove("hide");
+  // highscores user information form appears to record score
+  // hides quiz
+  userScore.textContent = score;
+}
+å;
+
+function handleScoreSubmit(event) {
+  event.preventDefault();
+  var userInitials = userInfo.value;
+  // console.log(userInitials);
+  var scoresArray = localStorage.getItem("scores");
+  if (scoresArray) {
+    scoresArray = JSON.parse(scoresArray);
+  } else {
+    scoresArray = [];
+  }
+  scoresArray.push({
+    userInitials,
+    score,
+  });
+  localStorage.setItem("scores", JSON.stringify(scoresArray));
+
+  // navigate to highscores.html
+  location.href = "../highscores.html";
+}
+
 //
 //
-// var answerKey
-// to-do: increase val of q index + 1
-// to-do: recall selectQuestion
-
-// console.log(choice);
-
-// // COMMANDS
-// // COMMANDS
-// // COMMANDS
-//
-
-// NOTE: introductory alert; displays upon visiting the page
-alert(
-  "¡Bienvenidos! Ready to test your JavaScript knowledge? Click 'Start Quiz' and see if you can beat the high score!"
-);
-
 // // EVENT LISTENERS
 // // EVENT LISTENERS
 // // EVENT LISTENERS
-//
 
 // NOTE: connects JS code to the HTML start button; triggers the function to begin the quiz
 startButton.addEventListener("click", startQuiz);
+submitButton.addEventListener("click", handleScoreSubmit);
